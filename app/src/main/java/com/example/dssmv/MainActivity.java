bb4a48233da6f5c2d34e010c5476b5b276760407
Rafael Moreira <1201674@isep.ipp.pt>
2021-11-20 17:42:36 +0000

    initial commit

---
+++ app/src/main/java/com/example/dssmv/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dssmv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
